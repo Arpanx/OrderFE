0324cee04e64e3c0423feba8aca204209a5f7739
Aleksandr <aleksandr.pisetskij@gmail.com>
2018-01-04 15:25:43 +0200

    +

--- src/angularApp/app/order/table-items/items-editable-row/items-editable-row.component.ts
+++ src/angularApp/app/order/table-items/items-editable-row/items-editable-row.component.ts
@@ -1,14 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-items-editable-row',
     templateUrl: 'items-editable-row.component.html',
     styleUrls: ['items-editable-row.component.scss']
 })
-export class ItemsEditableRowComponent {
+export class ItemsEditableRowComponent implements OnInit {
+    // Property for the gender
+    // private types: string[];
+    types: string[] = ['DillerGold', 'DillerSilver', 'Social', 'Retail'];
+
     @Input() item: { id: number, productName: string, status: string, location: string, type: string };
     @Output() onToggleDisplay = new EventEmitter<any>();
     @Output() onDeleteOrder = new EventEmitter<any>();
+
+    ngOnInit() {
+        this.types =  ['DillerGold', 'DillerSilver', 'Social', 'Retail'];
+        console.log(this.types);
+    }
+
+    onChange(event: any) {
+        this.item.type = event;
+    }
 
     toggleToDisplay() {
         this.onToggleDisplay.emit();
